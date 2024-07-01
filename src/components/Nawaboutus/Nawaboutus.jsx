6c97f94b08e99b02eaa3ed5cf06b672612cf2054
ttohaha <108796182+ttohaha@users.noreply.github.com>
2024-07-01 10:50:17 +0300

    try_2

--- src/components/Nawaboutus/Nawaboutus.jsx
+++ src/components/Nawaboutus/Nawaboutus.jsx
@@ -5,10 +5,13 @@ import img_picAvtor from '../../img/picAvtor.png'
 import img_picText from '../../img/picText.png'
 import img_pusAvtor from '../../img/pusAvtor.png'
 import img_pusText from '../../img/pusText.png'
+import img_backNav from '../../img/backheader.JPG'
+
 
 function Nawaboutus() {
   return (
     <div className="nawaboutus">
+        <img src={img_backNav} alt="" className="nawaboutus_back" />
         <div className="nawaboutus_left nawaboutus_box">
             <img src={img_picText} alt="" className="nawaboutus_left_avt nawaboutus_img_1" />
             <img src={img_picAvtor} alt="" className="nawaboutus_left_avt nawaboutus_img_2" />
