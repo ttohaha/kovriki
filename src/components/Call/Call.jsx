f56e9cb554ba974987ad2ce380dc890789556091
ttohaha <108796182+ttohaha@users.noreply.github.com>
2024-07-07 17:41:20 +0300

    try_7

--- src/components/Call/Call.jsx
+++ src/components/Call/Call.jsx
@@ -6,6 +6,7 @@ import img_bg_call1 from '../../img/bg_call1.JPG'
 import img_bg_call2 from '../../img/bg_call2.JPG'
 import img_call_cover from '../../img/call_cover.png'
 import img_call_men from '../../img/call_men.png'
+import img_call_men2 from '../../img/call_men2.png'
 import img_call_women from '../../img/call_women.png'
 
 
@@ -18,7 +19,7 @@ function Call() {
                 <div className="call_text">
                     <div className="call_text_flex">Давай подытожим: c коврами поможем. <br/> Твой лого умеем на всё наносить.<br/>А чтобы подробно узнать, что мы можем,<br/>Тебе только надо набрать и спросить.</div>
                 </div>
-                <img src={img_call_men} alt="" className="call_men_img_mobile" />
+                <img src={img_call_men2} alt="" className="call_men_img_mobile" />
             </div>
             {/* <img src={img_call_cover} alt="" className="call_cover_img" /> */}
             <img src={img_call_men} alt="" className="call_men_img" />
