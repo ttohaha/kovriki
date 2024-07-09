6e589bd4c8f41b8471de9312965c6419f2814e7c
ttohaha <108796182+ttohaha@users.noreply.github.com>
2024-07-09 15:53:29 +0300

    try_7

--- src/components/Phone/Phone.jsx
+++ src/components/Phone/Phone.jsx
@@ -16,7 +16,8 @@ function Phone() {
                 <div className="tooltip tooltip1">Whatsapp</div>
             </li>
             <li className="phone_block_content phone_block_content2">
-                <a className='phone_a_color2' href="tel:+375295008098" aria-label="Spotify" data-social="callme">
+                {/* <a className='phone_a_color2' href="tel:+375295008098" aria-label="Spotify" data-social="callme"> */}
+                <a className='phone_a_color2' href="" aria-label="Spotify" data-social="callme">
                     <div className="phone_filled"></div>
                     <img src={phone_call_img} alt="" className="watsapp_img" />
                 </a>
