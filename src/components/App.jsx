93033c23273a5aed4a96993d9c834a592950093f
ttohaha <108796182+ttohaha@users.noreply.github.com>
2024-07-09 20:02:14 +0300

    try_7

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,48 @@
 import {React, lazy, Suspense} from 'react'
 
 import './App.css';import Loader from './Loader/Loader';
-;
 
-const Homepage = lazy(()=> import('./Render_home/Homepage'))
+// import Navname from './Navname/Navname';
+// import Nawaboutus from './Nawaboutus/Nawaboutus';
+// import Price from './Price/Price';
+// import Cdek from './Cdek/Cdek';
+// import Evro from './Evro/Evro';
+// import Expi from './Expi/Expi';
+// import Call from './Call/Call';
+// import Phone from './Phone/Phone';
+// import Wood from './Wood/Wood';
+
+const Navname = lazy(()=> import('./Navname/Navname'))
+const Nawaboutus = lazy(()=> import('./Nawaboutus/Nawaboutus'))
+const Price = lazy(()=> import('./Price/Price'))
+const Cdek = lazy(()=> import('./Cdek/Cdek'))
+const Evro = lazy(()=> import('./Evro/Evro'))
+const Expi = lazy(()=> import('./Expi/Expi'))
+const Call = lazy(()=> import('./Call/Call'))
+const Phone = lazy(()=> import('./Phone/Phone'))
+const Wood = lazy(()=> import('./Wood/Wood'))
 
 function App() {
 
   return (
     <div className="App">
       <Suspense fallback={<Loader/>}>
-        <Homepage/>
+        <Phone/>
+        {/* <Navname/>
+        <Nav2aboutus/> */}
+        <div className="navbar">
+            <Navname/>
+            <Nawaboutus/>
+        </div>
+        <Price/>
+        <Cdek/>
+        <Evro/>
+        <Expi/>
+        <Call/> 
+        <Wood/>
       </Suspense>
     </div>
-  );
+  )
 }
 
 export default App;
