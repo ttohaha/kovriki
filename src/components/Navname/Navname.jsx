13baadf1f04d737934c5e178faf4dc0303f8dd3a
ttohaha <108796182+ttohaha@users.noreply.github.com>
2024-07-17 16:59:42 +0300

    first commit

--- src/components/Navname/Navname.jsx
+++ src/components/Navname/Navname.jsx
@@ -1,35 +1,43 @@
 import React from 'react'
 
 import './Navname.css';
-import whatsapp_img from '../../img2/watsapp.png'
-import phone_call_img from '../../img2/phone_call.png'
-
-function Navname() {
-  return (
-    <div className='navname'><div className="navname_text">Коврики.ру</div></div>
-  )
-}
+import whatsapp_img from '../../img2/watsapp2.png'
+import phone_call_img from '../../img2/phone_call2.png'
+import phone_mail_img from '../../img2/mail.png'
 
 // function Navname() {
 //   return (
-//     <div className='navname'>
-//       <div className="navname_block_phone">
-//         <div className="navname_block_phone1">
-//           <img src={phone_call_img} alt="" className="navname_img" />
-//           <div className="navname_number">89637113738</div>
-//         </div>
-//         <div className="navname_block_phone1">
-//           <img src={whatsapp_img} alt="" className="navname_img" />
-//           <div className="navname_number">89660956060</div>
-//         </div>
-//       </div>
-
-//       <div className="navname_email">
-//       <img src={phone_call_img} alt="" className="navname_img" />
-//       <div className="navname_email">kovriki2005@mail.ru</div>
-//       </div>
-//     </div>
+//     <div className='navname'><div className="navname_text">Коврики.ру</div></div>       
 //   )
 // }
+
+function Navname() {
+  return (
+    <div className='navname'>
+      <div className="navname_block_phone">
+        <a className='navname_block_phone1' href="tel:+375295008097">
+          <img src={phone_call_img} alt="" className="navname_img" />
+          <div className="navname_number">+89637113738</div>
+        </a>
+
+        <a className='navname_block_phone2' href="http://wa.me/89660956060" target='blank'>
+          <img src={whatsapp_img} alt="" className="navname_img" />
+          <div className="navname_number">Напимать в Watsapp</div>
+        </a>
+      </div>
+
+      {/* <div className="navname_block_phone3">
+        <img src={phone_mail_img} alt="" className="navname_img" />
+        <div className="navname_email">kovriki2005@mail.ru</div>
+      </div> */}
+
+      <a className='navname_block_phone3' href="mailto:loboda.toni@internet.ru">
+        <img src={phone_mail_img} alt="" className="navname_img" />
+        <div className="navname_email">kovriki2005@mail.ru</div>
+      </a>
+      {/* <a href="mailto:loboda.toni@interest.ru">vlad@interest.ru</a> */}
+    </div>
+  )
+}
 
 export default Navname
