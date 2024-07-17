88d42d9e8835ac82fdc9a45b18787d8bb6ff0dbb
ttohaha <108796182+ttohaha@users.noreply.github.com>
2024-07-17 13:25:11 +0300

    first commit

--- src/components/Navname/Navname.jsx
+++ src/components/Navname/Navname.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 
 import './Navname.css';
+import whatsapp_img from '../../img2/watsapp.png'
+import phone_call_img from '../../img2/phone_call.png'
 
 function Navname() {
   return (
     <div className='navname'><div className="navname_text">Коврики.ру</div></div>
   )
 }
+
+// function Navname() {
+//   return (
+//     <div className='navname'>
+//       <div className="navname_block_phone">
+//         <div className="navname_block_phone1">
+//           <img src={phone_call_img} alt="" className="navname_img" />
+//           <div className="navname_number">89637113738</div>
+//         </div>
+//         <div className="navname_block_phone1">
+//           <img src={whatsapp_img} alt="" className="navname_img" />
+//           <div className="navname_number">89660956060</div>
+//         </div>
+//       </div>
+
+//       <div className="navname_email">
+//       <img src={phone_call_img} alt="" className="navname_img" />
+//       <div className="navname_email">kovriki2005@mail.ru</div>
+//       </div>
+//     </div>
+//   )
+// }
 
 export default Navname
